042b6f39481cc78b72747c0b3c359a6334391560
Leopold <leopolds2010@gmail.com>
2026-05-01 20:10:08 +0300

    Expand plain text sitemap

--- api/sitemap-text.js
+++ api/sitemap-text.js
@@ -1,5 +1,13 @@
 export default function handler(_request, response) {
   response.setHeader('Content-Type', 'text/plain; charset=utf-8');
   response.setHeader('Cache-Control', 'public, max-age=0, must-revalidate');
-  return response.status(200).send('https://spine-link.vercel.app/\nhttps://spine-link.vercel.app/spine-preview.html\n');
+  return response.status(200).send([
+    'https://spine-link.vercel.app/',
+    'https://spine-link.vercel.app/spine-preview.html',
+    'https://spine-link.vercel.app/sitemap.xml',
+    'https://spine-link.vercel.app/robots.txt',
+    'https://spine-link.vercel.app/googlec2ff3a8991d80229.html',
+    'https://spine-link.vercel.app/googlef7147f9e5c822059.html',
+    '',
+  ].join('\n'));
 }
